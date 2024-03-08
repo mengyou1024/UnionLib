4572404e60f6f34ccb85700a26ab5b7470197636
mengyou <1523038073@qq.com>
2024-03-08 14:38:50 +0800

    refactor: AScanType函数体移动到源文件

--- include_private/AScanType.hpp
+++ include_private/AScanType.hpp
@@ -63,109 +63,33 @@ namespace Union::AScan {
          *
          * @return optional([最高回波位置(0-1.0), 回波强度(0-255)])
          */
-        std::optional<std::tuple<double, uint8_t>> GetGateResult(int gateIndex = 0, bool find_center_if_overflow = true) const {
-            if (gateIndex > this->gate.size()) {
-                return std::nullopt;
-            }
-            const auto &_data  = this->ascan;
-            const auto &_gate  = this->gate[gateIndex];
-            auto        start = _gate.pos;
-            double      end   = (double)(_gate.pos + _gate.width);
-            if (_data.size() < 100 || _gate.width <= 0.001 || std::abs(start - 1.0) < 0.00001 || _gate.pos < 0.0 || end > 1.0 || !_gate.enable) {
-                return std::nullopt;
-            }
-            auto left  = _data.begin() + static_cast<int64_t>((double)_data.size() * (double)_gate.pos);
-            auto right = _data.begin() + static_cast<int64_t>((double)_data.size() * (double)(_gate.pos + _gate.width));
-            auto max   = std::max_element(left, right);
-
-            if (find_center_if_overflow && *max == 255) {
-                std::vector<decltype(left)> minMaxVec = {};
-                for (auto &_left = left, &_right = right; _left != _right; _left++) {
-                    if (*_left == 255) {
-                        minMaxVec.push_back(_left);
-                    }
-                }
-                max = minMaxVec[0] + minMaxVec.size() / 2;
-            }
-            auto pos = ((double)std::distance(_data.begin(), max) / (double)_data.size());
-            if (pos < 0.0f) {
-                return std::nullopt;
-            }
-            return std::make_tuple(pos, *max);
-        }
+        std::optional<std::tuple<double, uint8_t>> GetGateResult(int gateIndex = 0, bool find_center_if_overflow = true) const;
 
         /**
-         * @brief 获取坐标轴的生产范围
+         * @brief 获取坐标轴的声程范围
          *
          * @return [bias, bias + len]
          */
-        std::array<double, 2> GetAxisRange(void) const {
-            return std::array<double, 2>({axisBias, axisBias + axisLen});
-        }
+        std::array<double, 2> GetAxisRange(void) const;
 
-        std::function<double(double)> getAVGLineExpr(void) const {
-            auto func = std::bind(Union::EchoDbDiffOfHole, std::placeholders::_1, 2.0, std::placeholders::_2, 2.0);
-            return getLineExpr(avg->index, avg->value, {0.0, 160.0}, {0.0, 520.0}, func);
-        }
+        /**
+         * @brief 获取AVG曲线的表达式
+         * 
+         * @return
+         */
+        std::function<double(double)> getAVGLineExpr(void) const;
 
-        std::function<double(double)> getDACLineExpr(void) const {
-            return getLineExpr(dac->index, dac->value, {0.0, 106.59}, {0.0, 520.0});
-        }
+        /**
+         * @brief 获取DAC曲线的表达式
+         * @return
+         */
+        std::function<double(double)> getDACLineExpr(void) const;
 
     private:
         std::function<double(double)> getLineExpr(const std::vector<int> &_index, const std::vector<uint8_t> &value,
                                                   const std::array<double, 2>          &r_range      = {0.0, 105.0} /*TODO: 声程计算*/,
                                                   const std::array<double, 2>          &v_range      = {0, 520.0},
-                                                  std::function<double(double, double)> func_db_diff = Union::EchoDbDiffOfPlan) const {
-            // TODO: 曲线平滑
-            using Union::ValueMap;
-            auto TAG = QLoggingCategory("Union");
-
-            std::vector<double> index;
-            for (auto &it : _index) {
-                index.emplace_back(ValueMap((double)it, r_range, v_range));
-            }
-
-            return [=, this](double val) -> double {
-                val = ValueMap(val, {axisBias, axisBias + axisLen}, v_range);
-                if (val <= index[0]) {
-                    return value.at(0);
-                } else {
-                    std::pair<double, double>                first  = {};
-                    std::optional<std::pair<double, double>> second = std::nullopt;
-                    if (index.size() == 1) {
-                        first = {index[0], value[0]};
-                    } else {
-                        bool in_range = false;
-                        for (int i = 1; i < index.size(); i++) {
-                            if (val >= index[i - 1] && val <= index[i]) {
-                                first    = {index[i - 1], value[i - 1]};
-                                second   = {index[i], value[i]};
-                                in_range = true;
-                            }
-                        }
-                        if (!in_range) {
-                            first  = {index.back(), value.back()};
-                            second = std::nullopt;
-                        }
-                    }
-
-                    auto CalcluateValue = [=, this](std::pair<double, double> pt) -> double {
-                        auto delta_db = func_db_diff(val, pt.first);
-                        return Union::CalculateGainOutput(pt.second, delta_db);
-                    };
-
-                    if (second.has_value()) {
-                        auto value_first   = CalcluateValue(first);
-                        auto vallue_second = CalcluateValue(second.value());
-                        auto first_rate    = (val - first.first) / (second->first - first.first);
-                        auto second_rate   = (second->first - val) / (second->first - first.first);
-                        return value_first * second_rate + vallue_second * first_rate;
-                    }
-                    return CalcluateValue(first);
-                }
-            };
-        }
+                                                  std::function<double(double, double)> func_db_diff = Union::EchoDbDiffOfPlan) const;
     };
 
     struct AScanType {
@@ -182,15 +106,13 @@ namespace Union::AScan {
         double                 samplingDelay  = {}; ///< 采样延迟
         int                    channel        = {}; ///< 通道
         std::string            instrumentName = {}; ///< 仪器型号
-        #if __has_include("QtCore")
-            static QJsonObject    GetBoardParamJSON(const AScanType &val);
-            static QList<QPointF> GetAScanSeriesData(const AScanData &data, double softGain = 0.0);
-            QJsonObject    GetBoardParamJSON(void) const;
-            QList<QPointF> GetAScanSeriesData(int index = 0, double softGain = 0.0) const;
-        #endif
+#if __has_include("QtCore")
+        static QJsonObject    GetBoardParamJSON(const AScanType &val);
+        static QList<QPointF> GetAScanSeriesData(const AScanData &data, double softGain = 0.0);
+        QJsonObject           GetBoardParamJSON(void) const;
+        QList<QPointF>        GetAScanSeriesData(int index = 0, double softGain = 0.0) const;
+#endif
     };
-
-
 
     template <class T>
     std::optional<AScanType> CONVERT_TO_STANDARD_ASCAN_TYPE(const T &data) {
--- src/AScanType.cpp
+++ src/AScanType.cpp
@@ -3,6 +3,105 @@
 #include <tuple>
 
 namespace Union::AScan {
+
+    std::optional<std::tuple<double, uint8_t>> AScanData::GetGateResult(int gateIndex, bool find_center_if_overflow) const {
+        if (std::cmp_greater(gateIndex, this->gate.size())) {
+            return std::nullopt;
+        }
+        const auto& _data = this->ascan;
+        const auto& _gate = this->gate[gateIndex];
+        auto        start = _gate.pos;
+        double      end   = (double)(_gate.pos + _gate.width);
+        if (_data.size() < 100 || _gate.width <= 0.001 || std::abs(start - 1.0) < 0.00001 || _gate.pos < 0.0 || end > 1.0 || !_gate.enable) {
+            return std::nullopt;
+        }
+        auto left  = _data.begin() + static_cast<int64_t>((double)_data.size() * (double)_gate.pos);
+        auto right = _data.begin() + static_cast<int64_t>((double)_data.size() * (double)(_gate.pos + _gate.width));
+        auto max   = std::max_element(left, right);
+
+        if (find_center_if_overflow && *max == 255) {
+            std::vector<decltype(left)> minMaxVec = {};
+            for (auto &_left = left, &_right = right; _left != _right; _left++) {
+                if (*_left == 255) {
+                    minMaxVec.push_back(_left);
+                }
+            }
+            max = minMaxVec[0] + minMaxVec.size() / 2;
+        }
+        auto pos = ((double)std::distance(_data.begin(), max) / (double)_data.size());
+        if (pos < 0.0f) {
+            return std::nullopt;
+        }
+        return std::make_tuple(pos, *max);
+    }
+
+    std::array<double, 2> AScanData::GetAxisRange() const {
+        return std::array<double, 2>({axisBias, axisBias + axisLen});
+    }
+
+    std::function<double(double)> AScanData::getAVGLineExpr() const {
+        auto func = std::bind(Union::EchoDbDiffOfHole, std::placeholders::_1, 2.0, std::placeholders::_2, 2.0);
+        return getLineExpr(avg->index, avg->value, {0.0, 160.0}, {0.0, 520.0}, func);
+    }
+
+    std::function<double(double)> AScanData::getDACLineExpr() const {
+        return getLineExpr(dac->index, dac->value, {0.0, 106.59}, {0.0, 520.0});
+    }
+
+    std::function<double(double)> AScanData::getLineExpr(const std::vector<int>&               _index,
+                                                         const std::vector<uint8_t>&           value,
+                                                         const std::array<double, 2>&          r_range,
+                                                         const std::array<double, 2>&          v_range,
+                                                         std::function<double(double, double)> func_db_diff) const {
+        // TODO: 曲线平滑
+        using Union::ValueMap;
+        auto TAG = QLoggingCategory("Union");
+
+        std::vector<double> index;
+        for (auto& it : _index) {
+            index.emplace_back(ValueMap((double)it, r_range, v_range));
+        }
+
+        return [=, this](double val) -> double {
+            val = ValueMap(val, {axisBias, axisBias + axisLen}, v_range);
+            if (val <= index[0]) {
+                return value.at(0);
+            } else {
+                std::pair<double, double>                first  = {};
+                std::optional<std::pair<double, double>> second = std::nullopt;
+                if (index.size() == 1) {
+                    first = {index[0], value[0]};
+                } else {
+                    bool in_range = false;
+                    for (int i = 1; std::cmp_less(i, index.size()); i++) {
+                        if (val >= index[i - 1] && val <= index[i]) {
+                            first    = {index[i - 1], value[i - 1]};
+                            second   = {index[i], value[i]};
+                            in_range = true;
+                        }
+                    }
+                    if (!in_range) {
+                        first  = {index.back(), value.back()};
+                        second = std::nullopt;
+                    }
+                }
+
+                auto CalcluateValue = [=](std::pair<double, double> pt = {}) -> double {
+                    return Union::CalculateGainOutput(pt.second, func_db_diff(val, pt.first));
+                };
+
+                if (second.has_value()) {
+                    auto value_first   = CalcluateValue(first);
+                    auto vallue_second = CalcluateValue(second.value());
+                    auto first_rate    = (val - first.first) / (second->first - first.first);
+                    auto second_rate   = (second->first - val) / (second->first - first.first);
+                    return value_first * second_rate + vallue_second * first_rate;
+                }
+                return CalcluateValue(first);
+            }
+        };
+    }
+
 #if __has_include("QtCore")
     QJsonObject AScanType::GetBoardParamJSON(const AScanType& val) {
         QJsonObject ret;
