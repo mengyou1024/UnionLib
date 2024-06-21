7529a7ea89d3de81639c134d65cc7e5cbc74f7f3
mengyou <1523038073@qq.com>
2024-06-21 16:03:54 +0800

    chore: 增加线程安全的阻塞队列

---
+++ include_private/common/BlockingQueue.hpp
@@ -0,0 +1,37 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include <QMutex>
+#include <QQueue>
+#include <QThread>
+#include <QWaitCondition>
+
+template <typename T>
+class BlockingQueue {
+public:
+    BlockingQueue() {}
+    void put(const T& value) {
+        QMutexLocker locker(&m_mutex);
+        m_queue.enqueue(value);
+        m_condition.wakeOne(); // 唤醒等待队列中的一个线程(来自wait)
+    }
+    T take() {
+        QMutexLocker locker(&m_mutex);
+        while (m_queue.isEmpty()) {
+            m_condition.wait(&m_mutex);
+        }
+        return m_queue.dequeue();
+    }
+    bool isEmpty() const {
+        QMutexLocker locker(&m_mutex);
+        return m_queue.isEmpty();
+    }
+    int size() const {
+        QMutexLocker locker(&m_mutex);
+        return m_queue.size();
+    }
+
+private:
+    QQueue<T>      m_queue;
+    mutable QMutex m_mutex;
+    QWaitCondition m_condition;
+};
